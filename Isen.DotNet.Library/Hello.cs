0f35662f7bedb33f70bb1fe9918c4a0f0f18c3d5
AlexOuillon <alex.ouillon@outlook.fr>
2018-02-27 10:33:19 +0100

    Console, lib ..

---
+++ Isen.DotNet.Library/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Isen.DotNet.Library
+{
+    public class Hello
+    {
+        public static string Greet(string name){
+            string str = "hello " + name + " ! "; 
+            return str; 
+        }
+    }
+}
